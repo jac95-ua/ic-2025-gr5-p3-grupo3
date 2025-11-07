ff00445ea030cd200801fba22e26ac1179681b22
Javier <jac95@alu.ua.es>
2025-11-07 16:59:18 +0100

    build hecho y primera ejecucion en portatil javi

---
+++ P3IC/source/detect/build/src/CMakeFiles/detect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../detect"
+  "../detect.pdb"
+  "CMakeFiles/detect.dir/main.cc.o"
+  "CMakeFiles/detect.dir/main.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
